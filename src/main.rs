d12c05b16f9e9267b0635b34f10dfbc33209c8a4
rufevean <deerajmydearalways@gmail.com>
2024-02-13 22:40:32 +0530

    finished a simple lexer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::env;
+use std::fs;
+
+mod lexer; 
+
+use lexer::scan;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let name = &args[1];
+    let contents = fs::read_to_string(name);
+    match contents {
+        Ok(contents) => scan::scan(&contents),
+        Err(e) => println!("Error reading {}: {}", name, e),
+    }
+
 }
